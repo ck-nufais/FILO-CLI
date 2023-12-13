af69ac2628eb177be8c27bb208d9c09955e9d04a
ck-nufais <ck.nufiii@gmail.com>
2023-12-13 21:15:21 +0530

    added small backbone with argparse

--- main.py
+++ main.py
@@ -1,73 +1,42 @@
-import os
-import shutil
+import argparse
+from filo import Filo
+import pathlib
+import json
 
-data = {
-    "Web": [".html5", ".html", ".htm", ".xhtml"],
-    "Picture": [
-        ".jpeg", ".jpg", ".tiff", ".gif", ".bmp", ".png", ".bpg", "svg",
-        ".heif", ".psd"
-    ],
-    "Video": [
-        ".avi", ".mkv", ".flv", ".wmv", ".mov", ".mp4", ".webm", ".vob",
-        ".mng", ".qt", ".mpg", ".mpeg", ".3gp"
-    ],
-    "Document": [
-        ".oxps", ".epub", ".pages", ".docx", ".txt", ".pdf", ".doc", ".fdf",
-        ".ods", ".odt", ".pwi", ".xsn", ".xps", ".dotx", ".docm", ".dox",
-        ".rvg", ".rtf", ".rtfd", ".wpd", ".xls", ".xlsx", ".ppt", "pptx"
-    ],
-    "Compressed": [
-        ".a", ".ar", ".cpio", ".iso", ".tar", ".gz", ".rz", ".7z", ".dmg",
-        ".rar", ".xar", ".zip"
-    ],
-    "Audio": [
-        ".aac", ".aa", ".aac", ".dvf", ".m4a", ".m4b", ".m4p", ".mp3", ".msv",
-        "ogg", "oga", ".raw", ".vox", ".wav", ".wma"
-    ],
-    "python": [".py"]
-}
+##Type Checks
 
+#function for Path type checking
+def Pathcheck(msg,x):
+    x = pathlib.Path(x)
+    if not x.exists():
+        print(msg)
+        raise SystemExit(1)
+    else:
+        return x
 
-class Filo:
+def Filecheck(x):
+    x = pathlib.Path(x)
+    if not x.is_file() or not x.exists():
+        print("The file path not found or is invalid file")
+        raise SystemExit(1)
+    return x
 
-  def __init__(self,
-               path: str = os.getcwd(),
-               dest: str = os.getcwd(),
-               data: dict = data,
-               ignore: list = []) -> None:
-
-    self.ignore = ignore
-    self.computed = {
-        value.lower(): key
-        for key, values in data.items() for value in values
-    }
-    self.src = path
-    self.dest = dest
-
-
-  def CopyOperation(self, dest, src, filename):
-    if not os.path.exists(dest):
-      os.mkdir(dest)
-    shutil.copyfile(src, (os.path.join(dest, filename)))
-
-
-  def OrganiseDir(self):
-    nonTouched = []
-    for each in os.scandir(self.src):
-      if each.is_file():
-        list = os.path.splitext(each)
-        ext = list[1].lower()
-        filename = each.name
-        if (ext in self.computed) and filename not in self.ignore:
-          self.CopyOperation(os.path.join(self.dest, self.computed[ext]),
-                                 each.path, filename)
-        else:
-          nonTouched.append(ext)
-    return nonTouched
-
-
-
-if __name__ == "__main__":
-    test = Filo()
-    test.OrganiseDir()
+#base bone 
+parser = argparse.ArgumentParser(description="CLI tool for organizing files",prog="FILO-CLI")
+parser.add_argument("src" ,help="Source Path which needs to be Organized",type=lambda x:Pathcheck("Incorrect Source Path",x))
+parser.add_argument("dest",help="Destination Path for Organized files",type=lambda x:Pathcheck("Incorrect Destination Path",x))
+parser.add_argument("-l","--list",help="Pass the file that containes mapped folders with extensions",default=None,type=Filecheck)
+args = parser.parse_args()
+src = args.src
+dest = args.dest
+ext = args.list
+decode = None
+if ext != None:
+    with ext.open("r") as file:
+        try:
+            decode = json.load(file)
+        except json.decoder.JSONDecodeError:
+            print("The Passed File is not correctly formatted for python dictionary or json\nfor more info refers to github")
+            raise SystemExit(1)
+print(decode)
 
